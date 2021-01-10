f17445a15ea8b47412b072b49cdde1b8dd33185b
fariv indra <farivind@gmail.com>
2021-01-11 00:15:46 +0700

    testing

--- app-login/src/components/HomeComp.js
+++ app-login/src/components/HomeComp.js
@@ -1,5 +1,13 @@
 import React, { Fragment } from "react";
+import { Link} from "react-router-dom";
 
 export default function HomeComp() {
-  return <Fragment>Home</Fragment>;
+  return (
+    <Fragment>
+      <h1>Coba nested router</h1>
+      <Link className="nav-item active" to="/Coba">
+        <p className="loginbut">Coba weh</p>
+      </Link>
+    </Fragment>
+  );
 }
