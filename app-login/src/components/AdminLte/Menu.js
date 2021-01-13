b484cf600b47666fcc0565c4eef661a27d573067
fariv indra <farivind@gmail.com>
2021-01-13 16:24:00 +0700

    get local storage done

--- app-login/src/components/AdminLte/Menu.js
+++ app-login/src/components/AdminLte/Menu.js
@@ -1,165 +1,172 @@
-import React, { Fragment, useContext, useEffect, useState } from "react";
+import React, { Fragment, useContext, useEffect } from "react";
 import { Link, Redirect } from "react-router-dom";
 import { AuthContext } from "../../Router/App";
-import Header from './Header'
-import Content from './Content'
+import Header from "./Header";
+import Content from "./Content";
 import Footer from "./Footer";
 
 export default function Menu() {
   const { state, dispatch } = useContext(AuthContext);
 
-  const [item, setitem] = useState(state)
-  console.log(item);
-
   const timeOut = () => {
-    setTimeout(()=>{
-      console.log('token telah berakhir');
+    const data = localStorage.getItem("expires");
+    setTimeout(() => {
+      console.log("token telah berakhir");
       dispatch({
-        type: 'LOGOUT'
-      })
-    }, state.tokenExpires)
-  }
+        type: "LOGOUT",
+      });
+    }, JSON.parse(data));
+  };
 
+  //username
+  const namas = localStorage.getItem("username");
+  const nama = JSON.parse(namas);
 
-
-  useEffect(()=>{
-
-    const data = localStorage.getItem('token')
-    
-    if(data){
-      setitem(JSON.parse(data))
-     }
-    
-    },[])
-
-    useEffect(() => {
-      localStorage.setItem('token', JSON.stringify(item))
-    })
-
-  useEffect(()=>{
+  useEffect(() => {
     // eslint-disable-next-line
-    timeOut()
-  },[])
-  console.log(state.isAuthentication, 'ini');
+    timeOut();
+  }, []);
 
-  if (!state.isAuthentication){
-    return <Redirect to='/login' />
+  // const loggedIn = localStorage.getItem("");
+  if (!state.loggedIn) {
+    return <Redirect to="/login" />;
   }
+
+  // useEffect(()=>{
+
+  //   const data = localStorage.getItem('username')
+
+  //   if(data){
+  //     setitem(JSON.parse(data))
+  //    }
+
+  //   },[])
+
+  //   useEffect(() => {
+  //     localStorage.setItem('username', JSON.stringify(item))
+  //   })
+  //   console.log(item);
+  // console.log(state.isAuthentication, 'ini');
+
+  // if (isAuthentication){
+  //   return <Redirect to='/login' />
+  // }
 
   return (
     <Fragment>
       <Header />
-        {/* Main Sidebar Container */}
-        <aside className="main-sidebar sidebar-dark-primary elevation-4">
-          {/* Brand Logo */}
-          <Link to="#" className="brand-link">
-            <img
-              src="dist/img/magetan.png"
-              alt="Siskaperbapo"
-              className="brand-image img-circle elevation-3"
-              // style={{ opacity: ".8" }}
-            />
-            <span className="brand-text font-weight-light">SISKAPERBAPO</span>
-          </Link>
-          {/* Sidebar */}
-          <div className="sidebar">
-            {/* Sidebar user panel (optional) */}
-            <div className="user-panel mt-3 pb-3 mb-3 d-flex">
-              <div className="image">
-                <img
-                  src="dist/img/user2-160x160.jpg"
-                  className="img-circle elevation-2"
-                  alt="User Image"
-                />
-              </div>
-              <div className="info">
-                <Link to="#" className="d-block">
-                  Hello, <b>{state.username}</b>
-                </Link>
-              </div>
+      {/* Main Sidebar Container */}
+      <aside className="main-sidebar sidebar-dark-primary elevation-4">
+        {/* Brand Logo */}
+        <Link to="#" className="brand-link">
+          <img
+            src="dist/img/magetan.png"
+            alt="Siskaperbapo"
+            className="brand-image img-circle elevation-3"
+            // style={{ opacity: ".8" }}
+          />
+          <span className="brand-text font-weight-light">SISKAPERBAPO</span>
+        </Link>
+        {/* Sidebar */}
+        <div className="sidebar">
+          {/* Sidebar user panel (optional) */}
+          <div className="user-panel mt-3 pb-3 mb-3 d-flex">
+            <div className="image">
+              <img
+                src="dist/img/user2-160x160.jpg"
+                className="img-circle elevation-2"
+                alt="User Image"
+              />
             </div>
-            {/* Sidebar Menu */}
-            <nav className="mt-2">
-              <ul
-                className="nav nav-pills nav-sidebar flex-column"
-                data-widget="treeview"
-                role="menu"
-                data-accordion="false"
-              >
-                <li className="nav-item">
-                  <Link to="/dashboard" className="nav-link">
-                    <i className="nav-icon fas fa-tachometer-alt" />
-                    <p>Dashboard</p>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/update-harga" className="nav-link">
-                    <i className="nav-icon fas fa-money-check-alt" />
-                    <p>Update Harga</p>
-                  </Link>
-                </li>
-
-                <li className="nav-header">Data Master</li>
-                <li className="nav-item">
-                  <Link to="/data-bahan-pokok" className="nav-link">
-                    <i className="nav-icon fas fa-database" />
-                    <p>Data Bahan Pokok</p>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/data-pasar" className="nav-link">
-                    <i className="nav-icon fas fa-database" />
-                    <p>Data Pasar</p>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/data-kategori" className="nav-link">
-                    <i className="nav-icon fas fa-database" />
-                    <p>Data Kategori</p>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/data-satuan" className="nav-link">
-                    <i className="nav-icon fas fa-database" />
-                    <p>Data Satuan</p>
-                  </Link>
-                </li>
-
-                <li className="nav-header">Akun</li>
-                <li className="nav-item">
-                  <Link to="/data-pengguna" className="nav-link">
-                    <i className="nav-icon fas fa-id-card" />
-                    <p>Data Pengguna</p>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to="/pengaturan-akun" className="nav-link">
-                    <i className="nav-icon fas fa-user-cog" />
-                    <p>Pengaturan Akun</p>
-                  </Link>
-                </li>
-                <li className="nav-item">
-                  <Link to='/#'
-                    className="nav-link"
-                    onClick={() =>
-                      dispatch({
-                        type: "LOGOUT",
-                      })
-                    }
-                  >
-                    <i className="nav-icon fas fa-sign-out-alt" />
-                    <p>Keluar</p>
-                  </Link>
-                </li>
-              </ul>
-            </nav>
-            {/* /.sidebar-menu */}
+            <div className="info">
+              <Link to="#" className="d-block">
+                Hello, <b>{nama}</b>
+              </Link>
+            </div>
           </div>
-          {/* /.sidebar */}
-        </aside>
+          {/* Sidebar Menu */}
+          <nav className="mt-2">
+            <ul
+              className="nav nav-pills nav-sidebar flex-column"
+              data-widget="treeview"
+              role="menu"
+              data-accordion="false"
+            >
+              <li className="nav-item">
+                <Link to="/dashboard" className="nav-link">
+                  <i className="nav-icon fas fa-tachometer-alt" />
+                  <p>Dashboard</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/update-harga" className="nav-link">
+                  <i className="nav-icon fas fa-money-check-alt" />
+                  <p>Update Harga</p>
+                </Link>
+              </li>
 
-        <Content/>
-        <Footer/>
+              <li className="nav-header">Data Master</li>
+              <li className="nav-item">
+                <Link to="/data-bahan-pokok" className="nav-link">
+                  <i className="nav-icon fas fa-database" />
+                  <p>Data Bahan Pokok</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/data-pasar" className="nav-link">
+                  <i className="nav-icon fas fa-database" />
+                  <p>Data Pasar</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/data-kategori" className="nav-link">
+                  <i className="nav-icon fas fa-database" />
+                  <p>Data Kategori</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/data-satuan" className="nav-link">
+                  <i className="nav-icon fas fa-database" />
+                  <p>Data Satuan</p>
+                </Link>
+              </li>
+
+              <li className="nav-header">Akun</li>
+              <li className="nav-item">
+                <Link to="/data-pengguna" className="nav-link">
+                  <i className="nav-icon fas fa-id-card" />
+                  <p>Data Pengguna</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link to="/pengaturan-akun" className="nav-link">
+                  <i className="nav-icon fas fa-user-cog" />
+                  <p>Pengaturan Akun</p>
+                </Link>
+              </li>
+              <li className="nav-item">
+                <Link
+                  to="/#"
+                  className="nav-link"
+                  onClick={() =>
+                    dispatch({
+                      type: "LOGOUT",
+                    })
+                  }
+                >
+                  <i className="nav-icon fas fa-sign-out-alt" />
+                  <p>Keluar</p>
+                </Link>
+              </li>
+            </ul>
+          </nav>
+          {/* /.sidebar-menu */}
+        </div>
+        {/* /.sidebar */}
+      </aside>
+
+      <Content />
+      <Footer />
     </Fragment>
   );
 }
